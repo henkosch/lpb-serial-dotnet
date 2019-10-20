e2388ec6a1c06c823cb0fe0bd6bdb68f4c1dfadf
Henrik <henrik.schnell@prosupport.io>
2019-10-20 09:20:16 +0200

    Parse Message.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Reactive.Linq;
 
 namespace LpbSerialDotnet
 {
+    class Message
+    {
+        private byte[] telegram;
+
+        public byte MagicByte { get; set; }
+        public byte Length { get; set; }
+        public byte Destination { get; set; }
+        public byte Source { get; set; }
+        public byte[] Unknown1 { get; set; }
+        public byte Type { get; set; }
+
+        public Message(byte[] telegram)
+        {
+            this.telegram = telegram;
+
+            using (var stream = new MemoryStream(telegram))
+            using (var reader = new BinaryReader(stream))
+            {
+                MagicByte = reader.ReadByte();
+                Length = reader.ReadByte();
+                Destination = reader.ReadByte();
+                Source = reader.ReadByte();
+                Unknown1 = reader.ReadBytes(4);
+                Type = reader.ReadByte();
+                // TODO: continue
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Dst: " + Program.SerialAddr(Destination) + 
+            ", Src: " + Program.SerialAddr(Source) +
+            ", Type: " + Type;
+        }
+    }
+
+    enum Address : byte
+    {
+        HEIZ = 0x00,
+        EM1 = 0x03,
+        EM2 = 0x04,
+        RGT1 = 0x06,
+        RGT2 = 0x07,
+        CNTR = 0x08,
+        DISP = 0x0A,
+        SRVC = 0x0B,
+        OZW = 0x31,
+        FE  = 0x32,
+        LAN = 0x42,
+        ALL = 0x7F,
+    }
+
+
     static class Program
     {
+        public static string SerialAddr(byte addr)
+        {
+            var value = (byte)(addr & 0x7F);
+            if (Enum.IsDefined(typeof(Address), value)) return ((Address)value).ToString();
+            else return ByteToHex(addr);
+        }
+
+        static Message ToMessage(byte[] telegram)
+        {
+            return new Message(telegram);
+        }
+
         static void Main(string[] args)
         {
             var serialPort = new SerialPort()
@@ -27,19 +93,32 @@ namespace LpbSerialDotnet
                 .ToByteStream()
                 .Select(InvertByte)
                 .ToTelegrams()
-                .ForEachAsync(PrintBytes)
+                .Select(PrintBytes)
+                .Select(ToMessage)
+                .ForEachAsync(PrintMessage)
                 .Wait();
         }
 
         static byte InvertByte(byte input) => (byte)(input^0xff);
 
-        static void PrintBytes(byte[] data)
+        static void PrintMessage(Message message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static byte[] PrintBytes(byte[] data)
         {
             Console.WriteLine(ByteArrayToHexString(data));
+            return data;
+        }
+
+        static string ByteToHex(byte data) 
+        {
+            return string.Format("{0:x2}", data);
         }
 
         static string ByteArrayToHexString(byte[] data) {
-            return string.Join("", data.Select(b => string.Format("{0:x2}", b)));
+            return string.Join("", data.Select(ByteToHex));
         }
     }
 }
